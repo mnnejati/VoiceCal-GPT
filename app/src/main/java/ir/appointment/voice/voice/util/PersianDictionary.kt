71a382d5e7b07d286e247acfe4ccd172545c99e1
mnnejati <nejati.mansour@gmail.com>
2026-08-06 20:11:26 +0330

    Create PersianDictionary.kt

---
+++ app/src/main/java/ir/appointment/voice/voice/util/PersianDictionary.kt
@@ -0,0 +1,29 @@
+package ir.appointment.voice.voice.util
+
+object PersianDictionary {
+
+    val doctorPrefixes = listOf(
+        "دکتر",
+        "دكتر",
+        "پزشک",
+        "دندانپزشک"
+    )
+
+    val personPrefixes = listOf(
+        "آقای",
+        "خانم",
+        "مهندس",
+        "استاد"
+    )
+
+    val locationKeywords = listOf(
+        "بیمارستان",
+        "کلینیک",
+        "درمانگاه",
+        "مطب",
+        "شرکت",
+        "اداره",
+        "دانشگاه",
+        "بانک"
+    )
+}
